f88b203aa55db319f2c7056f4ef054a5d4659b9c
Marta SH <msastreharo@gmail.com>
2021-05-31 13:53:42 +0200

    Initial commit

---
+++ src/main/java/org/factoriaf5/libritos/controllers/BookController.java
@@ -0,0 +1,29 @@
+package org.factoriaf5.libritos.controllers;
+
+import org.factoriaf5.libritos.models.Book;
+import org.factoriaf5.libritos.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    String listBooks(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("title", "Book list");
+        model.addAttribute("books", books);
+        return "books/all";
+    }
+}
